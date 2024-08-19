ad3ac3a53a021e829b985578a91cb011817c6bc3
Komputer Domowy <KrzychuK1211@gmail.com>
2024-08-19 21:23:46 +0200

    Creating file responsible for configuring Firebase and exporting Firestore for database operations.

---
+++ src/Firestore.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-SK07iPhPpJ7w8Nbt9yQRxFUzVi7h1k4",
+  authDomain: "polish-scheduler.firebaseapp.com",
+  projectId: "polish-scheduler",
+  storageBucket: "polish-scheduler.appspot.com",
+  messagingSenderId: "504785691352",
+  appId: "1:504785691352:web:ec5b9cf433ea7d1aaeadad"
+};
+
+const app = initializeApp(firebaseConfig);
+export default getFirestore(app);
